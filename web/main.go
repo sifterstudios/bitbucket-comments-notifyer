8302d4a75779e31ef4dab9f6fabf353793b87817
Tormod Liseth <tormod.liseth@klp.no>
2023-08-19 22:48:10 +0200

    Hello world web server!

--- web/main.go
+++ web/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/config.html")
+	})
+
+	port := 8080
+	fmt.Printf("Listening on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
