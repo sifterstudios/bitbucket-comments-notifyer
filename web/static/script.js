cb546a8052333880566afe6198eafba9d6117162
Tormod Liseth <tormod.liseth@klp.no>
2023-08-21 22:28:38 +0200

    Update Get call to use async function, rename button text in FE

--- web/static/script.js
+++ web/static/script.js
@@ -1,84 +1,83 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Elements
-    const monitoringFrequency = document.getElementById("monitoring-frequency");
-    const notifyComments = document.getElementById("notify-comments");
-    const notifyTasks = document.getElementById("notify-tasks");
-    const notifyStatus = document.getElementById("notify-status");
-    const saveButton = document.getElementById("save-button");
-    const lastUpdate = document.getElementById("last-update");
-    const activePRComments = document.getElementById("active-pr-comments");
-    const activePRTasks = document.getElementById("active-pr-tasks");
-    const manualButton = document.getElementById("update-button");
-    const testButton = document.getElementById("test-button");
+  // Elements
+  const monitoringFrequency = document.getElementById("monitoring-frequency");
+  const notifyComments = document.getElementById("notify-comments");
+  const notifyTasks = document.getElementById("notify-tasks");
+  const notifyStatus = document.getElementById("notify-status");
+  const saveButton = document.getElementById("save-button");
+  const lastUpdate = document.getElementById("last-update");
+  const activePRComments = document.getElementById("active-pr-comments");
+  const activePRTasks = document.getElementById("active-pr-tasks");
+  const manualButton = document.getElementById("update-button");
+  const testButton = document.getElementById("test-button");
 
-    // Save Configuration
-    saveButton.addEventListener("click", function () {
-        const frequencyValue = monitoringFrequency.value;
-        const commentsChecked = notifyComments.checked;
-        const tasksChecked = notifyTasks.checked;
-        const statusChecked = notifyStatus.checked;
+  // Save Configuration
+  saveButton.addEventListener("click", function () {
+    const frequencyValue = monitoringFrequency.value;
+    const commentsChecked = notifyComments.checked;
+    const tasksChecked = notifyTasks.checked;
+    const statusChecked = notifyStatus.checked;
 
-        console.log("Frequency:", frequencyValue);
-        console.log("Notify on Comments:", commentsChecked);
-        console.log("Notify on Tasks:", tasksChecked);
-        console.log("Notify on Status Changes:", statusChecked);
+    console.log("Frequency:", frequencyValue);
+    console.log("Notify on Comments:", commentsChecked);
+    console.log("Notify on Tasks:", tasksChecked);
+    console.log("Notify on Status Changes:", statusChecked);
+  });
+
+  function updateStatistics() {
+    updateStatsFromBackend().then((data) => {
+      if (!!data) {
+        lastUpdate.textContent = new Date(
+          data.LastUpdate * 1000
+        ).toLocaleString();
+        activePRComments.textContent = data.NumberOfActivePrComments;
+        activePRTasks.textContent = data.NumberOfActivePrTasks;
+      }
     });
+  }
 
-    function updateStatistics() {
-        updateStatsFromBackend()
-            .then(data => {
-                if (!!data) {
-                    lastUpdate.textContent = new Date(data.LastUpdate * 1000).toLocaleString();
-                    activePRComments.textContent = data.NumberOfActivePrComments;
-                    activePRTasks.textContent = data.NumberOfActivePrTasks;
-                }
-            })
+  updateStatistics();
 
-    }
+  setInterval(updateStatistics, 30000);
 
+  manualButton.addEventListener("click", function () {
     updateStatistics();
+  });
 
-    setInterval(updateStatistics, 30000);
-
-    manualButton.addEventListener("click", function () {
-        updateStatistics()
-    });
-
-    testButton.addEventListener("click", function () {
-        fetch("/send-notification", {
-            method: "POST",
-        })
-            .then(response => response.json())
-            .then(data => {
-                alert(data.message);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    });
+  testButton.addEventListener("click", function () {
+    fetch("/send-notification", {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert(data.message);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 });
 
-function updateStatsFromBackend() {
-    return fetch("/manual-update", {
-        method: "GET",
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (!!data) {
-                return data;
-            } else {
-                alert("Response was empty, but without error");
-                return null;
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            return null;
-        });
+async function updateStatsFromBackend() {
+  try {
+    const response = await fetch("/manual-update", {
+      method: "GET",
+    });
 
+    if (!response.ok) {
+      throw new Error("Network response was not ok: " + response.status);
+    }
+
+    const data = await response.json();
+
+    if (data) {
+      return data;
+    } else {
+      alert("Response was empty, but without error");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
 }
