ec623f54cbdc01152ec2cef4c778c820a4a41282
Tormod Liseth <tormod.liseth@klp.no>
2023-08-29 23:34:45 +0200

    Added simple parsing of filepath before notification

--- notification/notification.go
+++ notification/notification.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SendNotification(headline, message string) error {
-	cmd := exec.Command("notify-send", headline, message, "-t", "0")
+	cmd := exec.Command("notify-send", headline, message, "-t", "0") // TODO: Add this as an option in front-end
 
 	err := cmd.Run()
 	if err != nil {
@@ -20,20 +20,22 @@ func NotifyAboutOpenedPr() {
 	fmt.Println("New PR opened!")
 }
 func NotifyAboutNewAnswer(authorName string, message string, filePath, prTitle string) {
-	fmt.Printf("New answer by %s on PR %s: %s\n", authorName, filePath, message)
+	filePath = parseFilePath(filePath)
+	fmt.Println("New answer!")
 	err := SendNotification(fmt.Sprintf("New answer by %s on PR %s", authorName, prTitle), fmt.Sprintf("%s/n %s", filePath, message))
 	if err != nil {
 		fmt.Println(err)
 	}
 }
+
 func NotifyAboutNewComment(authorName string, message string, filePath, prTitle string) {
-	fmt.Printf("New comment by %s on PR %s: %s\n", authorName, filePath, message)
+	filePath = parseFilePath(filePath)
+	fmt.Println("New comment!")
 	err := SendNotification(fmt.Sprintf("New comment by %s on PR %s", authorName, prTitle), fmt.Sprintf("%s/n %s", filePath, message))
 	if err != nil {
 		fmt.Println(err)
 	}
 }
-
 func NotifyAboutNewAmend() {
 	fmt.Println("New amend!")
 }
@@ -84,4 +86,14 @@ func NotifyAboutReviewed() {
 
 func NotifyAboutNewTask() {
 	fmt.Println("New task!")
+}
+
+func parseFilePath(path string) string {
+	lastSlashIndex := 0
+	for i, c := range path {
+		if string(c) == "/" {
+			lastSlashIndex = i
+		}
+	}
+	return path[lastSlashIndex+1:]
 }
